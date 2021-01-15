20f0a224dab9b1125e32c10d21ee2e0ed448d7e6
suhaelat <suhaelat118@gmail.com>
2021-01-15 12:03:33 +0530

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from.import views
+urlpatterns = [
+    path('ass1',views.fun1,name='ass1')
+]    
---
+++ myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def fun1(req):
+    return render(req,'ass1.html')
+
+
