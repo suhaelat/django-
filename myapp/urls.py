f1c9d3f20498404e9cd14de0016db3cae94da124
suha <suhaelat118@gmail.com>
2021-02-04 11:11:16 +0530

    5th commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('ass4',views.fun4,name='ass4'),
     path('ass5',views.fun5,name='ass5'),
     path('ass6',views.fun6,name='ass6')
-    path('css',views.funcss,name='css'),
-    path('bs',views.funbs,name='bs')
 ]    
